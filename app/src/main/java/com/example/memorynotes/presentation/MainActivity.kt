a5ad6abf6b76d89c0d088ec045f7dace0413c8d8
Danilo Pinheiro <danilosoarespinheiro@gmail.com>
2023-02-24 16:09:50 -0300

    Added fragments and layouts

--- app/src/main/java/com/example/memorynotes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/memorynotes/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.memorynotes.presentarion
+package com.example.memorynotes.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/example/memorynotes/presentation/NoteFragment.kt
@@ -0,0 +1,18 @@
+package com.example.memorynotes.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.memorynotes.R
+
+class NoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+}
