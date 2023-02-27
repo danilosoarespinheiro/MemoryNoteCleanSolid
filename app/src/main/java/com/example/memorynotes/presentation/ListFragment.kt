f16ce79f318dfca14dd4d0850e324c8d168d46ea
Danilo Pinheiro <danilosoarespinheiro@gmail.com>
2023-02-26 21:22:55 -0300

    Added Navigation components

--- app/src/main/java/com/example/memorynotes/presentation/ListFragment.kt
+++ app/src/main/java/com/example/memorynotes/presentation/ListFragment.kt
@@ -5,14 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.memorynotes.R
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import com.example.memorynotes.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addNote.setOnClickListener { goToNoteDetails() }
+    }
+
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action: NavDirections = ListFragmentDirections.actionGoToNote(id)
+        Navigation.findNavController(binding.noteListView).navigate(action)
     }
 }
--- app/src/main/java/com/example/memorynotes/presentation/NoteFragment.kt
+++ app/src/main/java/com/example/memorynotes/presentation/NoteFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.memorynotes.R
+import androidx.navigation.Navigation
+import com.example.memorynotes.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
 
+    private var _binding: FragmentNoteBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.checkButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
     }
 }
